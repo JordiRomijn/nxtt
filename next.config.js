1fdbaa17468c9eb2eea02b8a957dab69871e2a01
JordiRomijn <62539361+JordiRomijn@users.noreply.github.com>
2023-05-08 11:31:27 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   env: {
     API_BASE: process.env.API_BASE,
     API_KEY: process.env.API_KEY,
-    APP_NAME: "NXTT | Digitalen producten met impact",
+    APP_NAME: "NXTT | Digitale producten met impact",
   },
 };
